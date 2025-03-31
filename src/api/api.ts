aed00d4be87fe71d7c8e755f350d89b7d19d5033
Michael Starovoitov <mihatestform@gmail.com>
2025-03-31 19:56:57 +0300

    debug7

--- src/api/api.ts
+++ src/api/api.ts
@@ -57,7 +57,12 @@ export const kundenApi = {
     getAllKunden(token:string) {
         return instance.get('api/kundens/',
             { headers: { "Content-Type": "application/json", 'Authorization': `Token ${token}`} }
-        ).then(response => response.data)
+        ).then(response => {
+            return instance.get('api/kundens/',
+                { headers: { "Content-Type": "application/json", 'Authorization': `Token ${token}`} }
+            ).then(response => response.data)
+        })
+        
         // return kunden
     },
     changeKundenById(token:string, id: string, body: any) {
