fed7525c5cc27d1f218124d679f4c05d5c81a173
Michael Starovoitov <mihatestform@gmail.com>
2025-04-09 09:21:31 +0300

    coment notuse variable

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { materials, workerById, users, Arbeitsplane, kunden, solutionsFull, allWorkersData, dashboardData } from '../data/TestData'
+// import { materials, workerById, users, Arbeitsplane, kunden, solutionsFull, allWorkersData, dashboardData } from '../data/TestData'
 
 const instance = axios.create({
     baseURL: 'http://127.0.0.1:8000/',
